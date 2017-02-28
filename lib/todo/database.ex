1f29268decafecd9398392f251722d4f8bac7de4
Mike Zazaian <zazaian@gmail.com>
2017-02-28 09:30:11 -0500

    add database pooling

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -1,49 +1,42 @@
 defmodule Todo.Database do
   use GenServer
 
-  def start(db_folder) do
-    GenServer.start(__MODULE__, db_folder,
-      name: :database_server
-    )
+  def start(db_folder, total_workers \\ 3) do
+    GenServer.start(__MODULE__, {db_folder, total_workers}, name: :database_server)
   end
 
-  def store(key, data) do
-    GenServer.cast(:database_server, {:store, key, data})
-  end
-
-  def get(key) do
-    GenServer.call(:database_server, {:get, key})
-  end
-
-  def init(db_folder) do
+  def init({db_folder, total_workers}) do
     File.mkdir_p(db_folder)
-    {:ok, db_folder}
+    {:ok, start_workers(db_folder, total_workers)}
   end
 
-  def cleanup do
-    GenServer.cast(:database_server, :cleanup)
+  def store(list_name, list) do
+    list_name
+    |> choose_worker
+    |> Todo.Database.Worker.store(list_name, list)
   end
 
-  def handle_cast(:cleanup, db_folder) do
-    File.rm_rf(db_folder)
-    {:noreply, db_folder}
+  def get(list_name) do
+    list_name
+    |> choose_worker
+    |> Todo.Database.Worker.get(list_name)
   end
 
-  def handle_cast({:store, key, data}, db_folder) do
-    file_name(db_folder, key)
-    |> File.write!(:erlang.term_to_binary(data))
-
-    {:noreply, db_folder}
+  def choose_worker(list_name) do
+    GenServer.call(:database_server, {:get_worker, list_name})
   end
 
-  def handle_call({:get, key}, _, db_folder) do
-    data = case File.read(file_name(db_folder, key)) do
-      {:ok, contents} -> :erlang.binary_to_term(contents)
-      _ -> nil
-    end
-
-    {:reply, data, db_folder}
+  def handle_call({:get_worker, list_name}, _, workers) do
+    total_workers = workers |> Map.keys |> Enum.count
+    worker_index = :erlang.phash2(list_name, total_workers)
+    worker_pid = Map.get(workers, worker_index)
+    {:reply, worker_pid, workers}
   end
 
-  defp file_name(db_folder, key), do: "#{db_folder}/#{key}"
+  defp start_workers(db_folder, total) do
+    Enum.reduce(0..(total - 1), %{}, fn(index, workers) ->
+      worker_pid = Todo.Database.Worker.start(db_folder)
+      Map.put(workers, index, worker_pid)
+    end)
+  end
 end
---
+++ lib/todo/database/worker.ex
@@ -0,0 +1,37 @@
+defmodule Todo.Database.Worker do
+  use GenServer
+
+  def start(db_folder) do
+    GenServer.start(__MODULE__, db_folder)
+  end
+
+  def store(worker_pid, list_name, list) do
+    GenServer.cast(worker_pid, {:store, list_name, list})
+  end
+
+  def get(worker_pid, list_name) do
+    GenServer.call(worker_pid, {:get, list_name})
+  end
+
+  def handle_cast({:store, list_name, list}, db_folder) do
+    file_name(db_folder, list_name)
+    |> File.write!(:erlang.term_to_binary(list))
+
+    {:noreply, db_folder}
+  end
+
+  def handle_call({:get, list_name}, _, db_folder) do
+    list = case File.read(file_name(db_folder, list_name)) do
+      {:ok, contents} -> :erlang.binary_to_term(contents)
+      _ -> nil
+    end
+
+    {:reply, list, db_folder}
+  end
+
+  # Needed for testing purposes
+  def handle_info(:stop, state), do: {:stop, :normal, state}
+  def handle_info(_, state), do: {:noreply, state}
+
+  defp file_name(db_folder, list_name), do: "#{db_folder}/#{list_name}"
+end
